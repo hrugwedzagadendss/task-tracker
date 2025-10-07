2e6f84d68a010a0c50be0f6b13e83ccfcfd3978a
Hrugwed <hrugwed.zagade@ndsofttech.com>
2025-10-07 16:24:06 +0530

    Updated header text

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>Welcome lets solve the merge conflict"edited"</h2>
+      <h1>Task Tracker</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
