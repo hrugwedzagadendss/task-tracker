bdb4379567a27bc68d36bbcf0917c1ac5a13ab66
Hrugwed <hrugwed.zagade@ndsofttech.com>
2025-10-07 16:17:19 +0530

    Conflicting change in main

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Welcome lets solve the merge conflict"edited"</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
